ca8ac8036d48bc306349374ef639690d4b769c23
Hygor Rasec <hygorrasec@gmail.com>
2022-07-10 12:53:03 -0300

    learning request and response in urls.py

--- django_project01/urls.py
+++ django_project01/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+# HTTP REQUEST
+def myView(request):
+    # Servidor responde a request feita pelo cliente...
+    return HttpResponse('Estou respondendo com uma String!')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', myView)  # Cliente pediu uma request...
 ]
